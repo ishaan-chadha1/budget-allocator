c6b7fdb14f8b129224db257485158c2f346f3ffa
Ishaan Chadha <ishaan.chadha12@gmail.com>
2023-07-09 14:27:31 +0530

    Addition of Plus and Minus Functionality

--- src/components/AllocationItem.js
+++ src/components/AllocationItem.js
@@ -2,24 +2,18 @@ import React, { useContext } from 'react';
 import { BudgetContext } from '../context/BudgetContext';
 
 const AllocationItem = (props) => {
-  const { deleteAllocation, currency } = useContext(BudgetContext);
+  const { deleteAllocation, increaseAllocation, decreaseAllocation, currency } = useContext(BudgetContext);
 
   return (
     <tr>
       <td>{props.name}</td>
+      <td>{currency}{props.cost}</td>
       <td>
-        <span className='badge badge-primary badge-pill mr-3' style={{ color: 'black' }}>
-          {currency}
-          {props.cost}
-        </span>
+        <button onClick={() => increaseAllocation(props.id)} className='btn btn-success btn-sm'>+</button>
+        <button onClick={() => decreaseAllocation(props.id)} className='btn btn-warning btn-sm ml-2'>-</button>
       </td>
       <td>
-        <button
-          onClick={() => deleteAllocation(props.id)}
-          className='btn btn-danger btn-sm'
-        >
-          X
-        </button>
+        <button onClick={() => deleteAllocation(props.id)} className='btn btn-danger btn-sm'>X</button>
       </td>
     </tr>
   );
